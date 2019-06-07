3fc8fa9a98a55a8237fabd87a5bd3bc39e551a0c
Walmyr Lima <walmyr@gitlab.com>
2019-06-07 12:07:10 +0200

    Test cleanup

--- walmyr_filho_test.rb
+++ walmyr_filho_test.rb
@@ -9,40 +9,19 @@ describe "Walmyr's website" do
     driver = Selenium::WebDriver.for :chrome
     driver.navigate.to my_website_url
 
-    # Sleeping just for the fun of watching the test run.
-    sleep 1
-
     youtube_social_icon = driver.find_element(css: 'img[alt="YouTube"]')
     driver.action.move_to(youtube_social_icon).perform
-
-    # Sleeping just for the fun of watching the test run.
-    sleep 1
-
     youtube_social_icon.click
-
-    # Sleeping just for the fun of watching the test run.
-    sleep 1
 
     driver.switch_to.window(driver.window_handles[1])
 
     expect(driver.current_url).to eq("https://www.youtube.com/user/wlsf82/videos")
 
-    # Sleeping just for the fun of watching the test run.
-    sleep 1
-
     driver.switch_to.window(driver.window_handles[0])
 
     expect(driver.current_url).to eq(my_website_url)
 
-    # Sleeping just for the fun of watching the test run.
-    sleep 1
-
-    header = driver.find_element(css: 'h1')
-    header.click
-
-    # Sleeping just for the fun of watching the test run.
-    sleep 1
-
+    driver.find_element(css: 'h1').click
     driver.quit
   end
 end
